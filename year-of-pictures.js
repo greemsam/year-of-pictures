ca3b80af6cec3a17794f002326ad6819591accc8
Sungyoon Jung <30921903+greemsam@users.noreply.github.com>
2022-12-30 15:05:09 +0900

    Add files via upload

--- year-of-pictures.js
+++ year-of-pictures.js
@@ -18,17 +18,37 @@ const picturesByYear = {
 	nov:$('#nov'),
 	dec:$('#dec')
 } 
+const imgListBackgrounds = [
+	'rgba(82, 80, 206, 1)',
+	'rgba(82, 80, 206, 0.5)',
+	'rgba(82, 80, 206, 1)',
+	'rgba(82, 80, 206, 0.5)',
+	'rgba(82, 80, 206, 0.5)',
+	'rgba(82, 80, 206, 1)',
+	'rgba(82, 80, 206, 0.5)',
+	'rgba(82, 80, 206, 1)',
+	'rgba(82, 80, 206, 1)',
+	'rgba(82, 80, 206, 0.5)',
+	'rgba(82, 80, 206, 1)',
+	'rgba(82, 80, 206, 0.5)',
+] 
+const imgListArray = document.querySelectorAll('.img-lists__element')
+const imgListMonthArray = document.querySelectorAll('.month')
 
+$('h1').innerHTML = `My Drawings in ${new Date().getFullYear()}`
 for(const key of Object.keys(picturesByYear)){
 	const imgTagLists = $(`#${key}-img`)
 	picturesByYear[key].addEventListener('change', (e)=>{
 		const targetFilesArray = Array.from(e.target.files)
 		const selectedFile = targetFilesArray.map(file => {return URL.createObjectURL(file)})
 		imgTagLists.setAttribute('src', selectedFile)
+		imgTagLists.classList.add('updated')
 	})
 }
 
-
+for(let i = 0; i<imgListArray.length; i++){
+	imgListArray[i].style.background = imgListBackgrounds[i]
+}
 const saveAs = (url, fileName) =>{
 	// 캡쳐된 파일을 이미지 파일로 내보낸다.
 	var link = document.createElement('a'); 
